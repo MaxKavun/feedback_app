8c263d27721a28a6731ab3ed9d4e7ede63a95ce8
Max Kavun <maxkavun@outlook.com>
2019-08-29 15:42:16 +0000

    Fix bugs

--- init.sql
+++ init.sql
@@ -1,1 +1,1 @@
-CREATE TABLE feedback.feedback(id INT PRIMARY KEY AUTO_INCREMENT,customer VARCHAR(255),dealer VARCHAR(255),rating VARCHAR(255),comments TEXT)";
+CREATE TABLE feedback.feedback(id INT PRIMARY KEY AUTO_INCREMENT,customer VARCHAR(255),dealer VARCHAR(255),rating VARCHAR(255),comments TEXT);
